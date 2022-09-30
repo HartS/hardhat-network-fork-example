1e91be2396dabdcd13bcf23ca9d7a8cad3792f9c
Hart Simha <hmsimha@gmail.com>
2022-09-30 12:03:01 -0700

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+dotenv = require('dotenv')
+dotenv.config()
+
+const { ALCHEMY_API_KEY } = process.env
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  namedAccounts: {
+    deployer: '0xf2E055D3204aD73C7C51DE2668435B76C727a92f',
+  },
+  networks: {
+    "localfork": {
+      url: "http://localhost:8545",
+      chainId: 31336,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 15645012,
+      },
+    },
+    "hardhat": {
+      chainId: 31336,
+      forking: {
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: 15645012,
+      },
+    },
+  },
+};
